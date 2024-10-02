e684e8c31f989e9f7087099b1d2aa93f4c94fa6a
jeyashrii <jeyashri30@gmail.com>
2024-10-02 23:51:44 +0530

    weather app

---
+++ weather app/script.js
@@ -0,0 +1,29 @@
+const apiKey = "76eb11efef494fa5fe9befcf81d4d028";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBar = document.querySelector(".search-input");
+const weatherIcon = document.querySelector(".weather-icon");
+
+const searchButton = document.querySelector(".search-img");
+
+async function getWeather(city) {
+  const response = await fetch(apiUrl + `${city}&appid=${apiKey}`);
+  var data = await response.json();
+  //console.log(data);
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+  const weatherCondition = data.weather[0].main;
+  weatherIcon.src = `images/${weatherCondition}.png`;
+
+  document.querySelector(".weather").style.display = "block";
+}
+
+searchButton.addEventListener("click", () => {
+  const city = searchBar.value.trim();
+  if (city === "") {
+    alert("please enter a city name");
+  }
+  getWeather(city);
+});
